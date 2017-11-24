a5cb9d8ed19a65a97681aeb031f8368409bf662e
Rahovec <konstantin.todorov98@gmail.com>
2017-11-24 22:04:30 +0200

    Adding libraries

---
+++ cpp/cpp_libs/testfile.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Lib.h"
+
+using namespace std;
+int main(){
+	
+	int a,b;
+	cout << "enter variables" << endl;
+	cin >> a >> b;
+	cout << "answer is:" << subtract(a,b) << endl;
+
+	return 0;
+}
