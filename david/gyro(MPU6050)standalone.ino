b7d72dd47f5799e9b3324d6554d5bbfacfee0ccd
Konstantin Todorov <konstantin.todorov98@Gmail.com>
2018-05-28 00:31:48 +0300

    Create gyro(MPU6050)standalone.ino
    samo jiroskopa

---
+++ david/gyro(MPU6050)standalone.ino
@@ -0,0 +1,36 @@
+#include "Wire.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+MPU6050 accelgyro;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+
+bool blinkState = false;
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    Wire.begin();
+    Serial.begin(115200);
+
+    Serial.println("Initializing I2C devices...");
+    accelgyro.initialize();
+
+    Serial.println("Testing device connections...");
+    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+}
+
+void loop() {
+    
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+    Serial.print("a/g:\t");
+    Serial.print(ax); Serial.print("\t");
+    Serial.print(ay); Serial.print("\t");
+    Serial.print(az); Serial.print("\t");
+    Serial.print(gx); Serial.print("\t");
+    Serial.print(gy); Serial.print("\t");
+    Serial.println(gz);
+}
