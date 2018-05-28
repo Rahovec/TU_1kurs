47012d91cc32d67a1126677d34a9bea2adca53f0
Konstantin Todorov <konstantin.todorov98@Gmail.com>
2018-05-28 12:24:54 +0300

    Update final.ino
    Adding GPS

--- david/final.ino
+++ david/final.ino
@@ -1,8 +1,9 @@
-
 #include <Wire.h>
 #include <SparkFunBME280.h>
 #include "I2Cdev.h"
 #include "MPU6050.h"
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
 
 MPU6050 accelgyro;
 
@@ -13,10 +14,15 @@ int16_t gx, gy, gz;
 bool blinkState = false;
 BME280 mySensor; //Uses I2C address 0x76 (jumper closed)
 
+TinyGPS gps;
+SoftwareSerial ss(1, 2); // RX|TX
+
+
 void setup()
 {
   Serial.begin(115200);
   Wire.begin();
+  ss.begin(4800);
 
   mySensor.setI2CAddress(0x76); //Connect to a second sensor
   if(mySensor.beginI2C() == false) Serial.println("BMP Sensor  connect failed");
@@ -51,5 +57,32 @@ void loop()
     Serial.print(gy); Serial.print("\t");
     Serial.println(gz);
 
-  delay(100);
+  Serial.println("====GPS====");
+  bool newData = false;
+  unsigned long chars;
+  unsigned short sentences, failed;
+
+  // For one second we parse GPS data and report some key values
+  for (unsigned long start = millis(); millis() - start < 100;)
+  {
+    while (ss.available())
+    {
+      char c = ss.read();
+      // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+      if (gps.encode(c)) // Did a new valid sentence come in?
+        newData = true;
+    }
+  }
+
+  gps.stats(&chars, &sentences, &failed);
+  Serial.print(" CHARS=");
+  Serial.print(chars);
+  Serial.print(" SENTENCES=");
+  Serial.print(sentences);
+  Serial.print(" CSUM ERR=");
+  Serial.println(failed);
+  if (chars == 0)
+    Serial.println("** No characters received from GPS: check wiring **");
+
+  delay(1000);
 }
